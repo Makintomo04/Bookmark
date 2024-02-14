54e81becaa60a1096ea414d2690da94a46621669
Michael Akintomo <1721809@alumni.brunel.ac.uk>
2024-02-14 01:53:24 +0000

    metadata update

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,9 +37,7 @@ export default async function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/favicon.ico" sizes="any"/>
         <link rel="icon" href="/icon?<generated>" type="image/<generated>" sizes="<generated>"/>
-        <link rel="apple-touch-icon" href="/apple-touch-icon?<generated>" type="image/<generated>" sizes="<generated>"/>
       </head>
       <body className={font.className}>
         <Providers>
